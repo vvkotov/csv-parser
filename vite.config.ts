88d50bff9d610955c0915d38a942410bf88a7f5f
Vitaliy Kotov <vitaliy.kotov@fairmarkit.com>
2025-07-01 18:58:44 +0200

    Bundle icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { barrel } from "vite-plugin-barrel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [
+    react(),
+    barrel({
+      packages: ["lucide-react"],
+    }),
+  ],
 });
